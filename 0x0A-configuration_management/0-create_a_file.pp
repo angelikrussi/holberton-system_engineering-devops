c6d0faa0a604dda79103a6ee3aaeb8992f65ed14
angelikrussi <hannamrrussi@gmail.com>
2022-01-03 16:54:51 -0500

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-#Create a file in /tmp
+# 0. Create a file
 
-file{'/tmp/school':
-  ensure     => file,
-  mode       => 0744,
-  owner      => www-data,
-  group      => www-data,
-  contains   => 'I love Puppet'
+file {'/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => www-data,
+  group   => www-data,
+  content => 'I love Puppet'
   }
+  
